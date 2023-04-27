f735fe2ef74c0acd32c378994450d7add514fb11
Kushal <kushalbhetuwal@gmail.com>
2023-04-27 15:07:16 +1000

    added the file with changes

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const button = document.querySelector('button');
 button.addEventListener('click',()=>{
     button.style.backgroundColor = 'blue';
 })
+
+const h2 = document.getElementsByTagName('h2')[0];
+h2.addEventListener("click",()=>{
+    h2.style.backgroundColor = "blue";
+})
